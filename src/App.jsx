f546c0f0de3e5a964013d5cb71ed145a2a429c0d
Ashrithgowda1901 <ashrithgowda04@gmail.com>
2025-12-11 21:34:37 +0530

    updated to  use map function
    updated to  use map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,47 @@
 import './App.css'
 import Card from './components/Card'
 
-function App(){
-  return(
-    <>
-      <div id="parent">
-        <Card name="Colosseum (Italy)" place="italy.jpg" />
-        <Card name="Christ the Redeemer (Brazil)" place="brazil.jpg" />
-        <Card name="Great Wall (China)" place="china.jpg" />
-        <Card name="Machu Picchu (Peru)" place="peru.jpg" />
-        <Card name="Petra (Jordan)" place="jordan.jpg" />
-        <Card name="Chichén Itzá (Mexico)" place="mexico.jpg" />
-        <Card name="Taj Mahal (India)" place="india.jpg" />
-      </div>
-    </>
-  )
+function App() {
+  let sevenWonders = [
+    {
+      name: "Colosseum (Italy)",
+      place: "italy.jpg"
+    },
+    {
+      name: "Christ the Redeemer (Brazil)",
+      place: "brazil.jpg"
+    },
+    {
+      name: "Great Wall (China)",
+      place: "china.jpg"
+    },
+    {
+      name: "Machu Picchu (Peru)",
+      place: "peru.jpg"
+    },
+    {
+      name: "Petra (Jordan)",
+      place: "jordan.jpg"
+    },
+    {
+      name: "Chichén Itzá (Mexico)",
+      place: "mexico.jpg"
+    },
+    {
+      name: "Taj Mahal (India)",
+      place: "india.jpg"
+    },
+  ]
+  // console.log(sevenWonders.map((sevenwonder)=>sevenwonder.id))
+  
+  return (
+    <div id="parent" >
+      {
+        sevenWonders.map((sevenWonder,index) => (
+          <Card key={index} name={sevenWonder.name} place={sevenWonder.place} />
+        ))
+      }
+    </div>
+  );
 }
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,14 @@
 import expandIcon from "/expand.jpg";
 import "./Card.css"
 
-const Card = ({name,place}) => {
+const Card = (props) => {
     return (
         <div className="card">
             <div className='expand'>
                 <img src={expandIcon} alt="" />
             </div>
-            <img src={place} alt="" />
-            <h2>{name})</h2>
+            <img src={props.place} alt="" />
+            <h2>{props.name}</h2>
             <p>Symbol of ancient Rome, the Colosseum (or Flavian Amphitheater), built between 72 and 80 AD, could host up to 50,000 spectators for gladiator fights, shows, and public games.</p>
         </div>
     )
